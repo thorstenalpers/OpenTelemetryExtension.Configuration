5345db4a2c697ee92ba368de21ccc1f7364cdadc
thorsten <thorsten@PC>
2026-06-12 18:52:29 +0200

    chore: rename aspire-dashboard helm files, drop empty openobserve nodeport template, use absolute README image URLs

--- infrastructure/helm/helm-install-aspire-dashboard.cmd
+++ infrastructure/helm/helm-install-aspire-dashboard.cmd
@@ -3,10 +3,10 @@ helm repo add aspire-dashboard https://kube-the-home.github.io/aspire-dashboard-
 helm repo update 
 
 helm uninstall aspire-dashboard
-helm install aspire-dashboard aspire-dashboard/aspire-dashboard --version 1.28.3  -f values.aspire-dashboard.yaml
+helm install aspire-dashboard aspire-dashboard/aspire-dashboard --version 1.28.3  -f aspire-dashboard.values.yaml
 
-kubectl delete -f nodeports.aspire-dashboard.yaml
-kubectl apply -f nodeports.aspire-dashboard.yaml
+kubectl delete -f aspire-dashboard.nodeports.yaml
+kubectl apply -f aspire-dashboard.nodeports.yaml
 
 REM helm show values aspire-dashboard/aspire-dashboard > values.aspire-dashboard.yaml
 
